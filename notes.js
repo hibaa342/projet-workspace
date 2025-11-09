578c4cbebb4749305575d057eec9658da10507b7
hajar <elberguihajar18@gmail.com>
2025-11-09 23:42:59 +0100

    versionfinal

--- notes.js
+++ notes.js
@@ -1,21 +1,25 @@
+/* créer une variable */
 let notes = [];
 function sauvegarderNote() {
   const zoneDeTexte = document.getElementById("noteInput");
+  /*Récupère le texte écrit dans l'input */
   const texteNote = zoneDeTexte.value.trim();
   if (texteNote === "") return;
-
+/*Méthode qui ajoute un élément à la fin du tableau*/
   notes.push(texteNote);
   zoneDeTexte.value = "";
   afficherNotes();
 }
-
+/* transformer le tableau en éléments HTML visibles*/
 function afficherNotes() {
   const listeNotes = document.getElementById("noteList");
   listeNotes.innerHTML = "";
-
+/*parcourt chaque élément du tableau*/
   notes.forEach((note) => {  
     const item = document.createElement("li"); 
+    /* Propriété pour écrire du texte dans un élément*/
      item.textContent = note;   
+     /* Ajoute l'élément */
    listeNotes.appendChild(item);
   });
 }
